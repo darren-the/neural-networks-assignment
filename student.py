ba459c9565d7e643fbfac6e3c7938f60d2635d18
PhoenixHSC <35628597+PhoenixHSC@users.noreply.github.com>
2021-07-26 14:32:19 +1000

    basic network

--- student.py
+++ student.py
@@ -48,24 +48,37 @@ def transform(mode):
 ############################################################################
 ######   Define the Module to process the images and produce labels   ######
 ############################################################################
+
 class Network(nn.Module):
     def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(2704, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 14)
-        
-    def forward(self, t):
-        t = self.pool(F.relu(self.conv1(t)))
-        t = self.pool(F.relu(self.conv2(t)))
-        t = torch.flatten(t, 1)
-        t = F.relu(self.fc1(t))
-        t = F.relu(self.fc2(t))
-        t = self.fc3(t)
-        return t
+        super(Network, self).__init__()
+        self.conv1=nn.Conv2d(in_channels=3, out_channels=6,kernel_size=5, padding=1) 
+        self.conv2=nn.Conv2d(in_channels=6,out_channels=12,kernel_size=5, padding=1) 
+        self.fc_layer_1=nn.Linear(12*60*60,250)
+        self.fc_layer_2=nn.Linear(250,14)
+
+    def forward(self, x):        
+
+        # (1) Convolutional Layer
+        x = F.relu(self.conv1(x))
+        # x = F.max_pool2d(x, kernel_size=2, stride=2)
+
+        # (2) Convolutional Layer
+        x = F.relu(self.conv2(x))
+        # x = F.max_pool2d(x, kernel_size=2, stride=2)
+
+        # (3) Linear Layer
+        x = x.flatten(start_dim=1)
+        x = F.relu(self.fc_layer_1(x))
+
+        # (4) Linear Layer
+        x = F.relu(self.fc_layer_2(x))
+
+        # (5) Output Layer
+        x = F.log_softmax(x, dim=1)  
+
+        return x  
+
 
 
 class loss(nn.Module):
@@ -90,4 +103,4 @@ dataset = "./data"
 train_val_split = 0.8
 batch_size = 256
 epochs = 60
-optimiser = optim.Adam(net.parameters(), lr=0.001)
+optimiser = optim.SGD(net.parameters(), lr=0.01, momentum=0.4)
